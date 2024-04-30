2eca0bbc30f86465938ee13a3d3545cfd2a2b180
Susanne Kreuzer <a11911757@univie.ac.at>
2024-04-30 11:11:54 +0200

    submit exam

--- main.py
+++ main.py
@@ -2,11 +2,33 @@
 Multiplication Table
 """
 # Provide your solution here
+def print_multiplication_table(number_table, number_row):
+
+
+
+
+
+
+
 
 """
 Palindromes
 """
 # Provide your solution here
+def is_palindrome(word: str) -> bool: return word == word[::-1]
+is_palindrome =
+
+if word in is_palindrome:
+    print (True)
+else: print (False)
+
+input_string = input("Please enter a word or phrase: ")
+index = 0
+while index < len(input_string)
+print(input_string[index])
+index +=1
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
